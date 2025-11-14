508acce1312e86747a4da6717138236d3db6c02a
mterkek <48011604+mterkek@users.noreply.github.com>
2025-11-14 20:01:33 +0300

    Add ground prefab and controllers, update ground material
    Introduced a new Ground prefab and related meta files, and added several ground-related controller scripts (GroundColourController, GroundDataTransmitter, GroundFallController, GroundPositionController, GroundSpawnController). Updated the ground material color and renamed it for consistency. Refactored SampleScene to use the prefab and new controllers, removing old ground objects and integrating color management and spawning logic.

--- ZigZag_GO/Assets/Scripts/Ground/GroundColourController.cs
+++ ZigZag_GO/Assets/Scripts/Ground/GroundColourController.cs
@@ -1,16 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundColourControl : MonoBehaviour
+public class GroundColorController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Material groundMaterial;
+    [SerializeField] private Color[] colours;
+    private int colorIndex = 0;
+    [SerializeField] private float lerpValue;
+    [SerializeField] private float time;
+    private float currentTime;
+
+    private void Update()
     {
-        
+        SetColorChangeTime();SetGroundMaterialSmoothColorChange();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetColorChangeTime()
     {
-        
+        if (currentTime <= 0)
+        {
+            CheckColorIndexValue();
+            currentTime = time;
+        }
+        else
+        {
+            currentTime -= Time.deltaTime;
+        }
     }
+
+    private void CheckColorIndexValue()
+    {
+        colorIndex++;
+
+        if (colorIndex >= colours.Length)
+        {
+            colorIndex = 0;
+        }
+    }
+
+    private void SetGroundMaterialSmoothColorChange()
+    {
+        groundMaterial.color = Color.Lerp(groundMaterial.color, colours[colorIndex], lerpValue * Time.deltaTime);
+    }
+    private void OnDestroy()
+{
+    groundMaterial.color = colours[1];
+}
+
 }
---
+++ ZigZag_GO/Assets/Scripts/Ground/GroundSpawnController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GroundSpawnController : MonoBehaviour
+{
+    public GameObject lastGroundObject;
+
+    [SerializeField] private GameObject groundPrefab;
+
+    private GameObject newGroundObject;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
