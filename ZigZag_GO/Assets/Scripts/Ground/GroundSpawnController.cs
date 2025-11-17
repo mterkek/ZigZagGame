7e785a3544a0dbece1a56d7ac1da70836ac326df
mterkek <48011604+mterkek@users.noreply.github.com>
2025-11-17 23:09:27 +0300

    Add GroundCollisionController and update ground spawning
    Introduced GroundCollisionController script and updated SampleScene to reference new ground prefab. Refactored GroundSpawnController to include ground spawning logic with randomized direction and batch creation of ground objects.

--- ZigZag_GO/Assets/Scripts/Ground/GroundSpawnController.cs
+++ ZigZag_GO/Assets/Scripts/Ground/GroundSpawnController.cs
@@ -8,16 +8,40 @@ public class GroundSpawnController : MonoBehaviour
 
     private GameObject newGroundObject;
 
+    private int groundDirection;
+    
+
+
+
 
 
     void Start()
     {
-        
+        SpawnGround();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SpawnGround()
     {
-        
+        CreateNewGround();
+        for (int i = 0; i < 90; i++)
+        {
+            CreateNewGround();
+        }
+    }
+    
+    private void CreateNewGround()
+    {
+    groundDirection = Random.Range(0, 2);
+        Vector3 newPosition;
+    
+        if (groundDirection == 0)
+        {
+            newPosition = lastGroundObject.transform.position + new Vector3(-1f, 0, 0);
+        }
+        else
+        {
+            newPosition = lastGroundObject.transform.position + new Vector3(0, 0, 1f);
+        }   
+    newGroundObject = Instantiate(groundPrefab, newPosition, Quaternion.identity);
+    lastGroundObject = newGroundObject;
     }
 }
